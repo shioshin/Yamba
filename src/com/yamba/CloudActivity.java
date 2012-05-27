d40d07fb846e24c190943436ed3f9514fca018b1
shioshin <shioshin@mail.com>
2012-05-27 14:54:33 +0300

    finished first activity with status and update.

---
+++ src/com/yamba/CloudActivity.java
@@ -0,0 +1,69 @@
+package com.yamba;
+
+import winterwell.jtwitter.Twitter;
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class CloudActivity extends Activity implements OnClickListener {
+	private static final String TAG = "StatusActivity";
+	EditText editText;
+	Button updateButton;
+	Twitter twitter;
+
+	@SuppressWarnings("deprecation")
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		editText = (EditText) findViewById(R.id.editText);
+		updateButton = (Button) findViewById(R.id.buttonUpdate);
+		updateButton.setOnClickListener(this);
+
+		twitter = new Twitter("student", "password");
+		twitter.setAPIRootUrl("http://yamba.marakana.com/api");
+	}
+
+	@Override
+	public void onClick(View v) {
+		String status = editText.getText().toString();
+		new PostToTwitter().execute(status); //
+		Log.d(TAG, "onClicked");
+	}
+
+	class PostToTwitter extends AsyncTask<String, Integer, String> {
+
+		@Override
+		protected String doInBackground(String... params) {
+			try {
+				winterwell.jtwitter.Status status = twitter
+						.updateStatus(params[0]);
+				return status.text;
+			} catch (Exception e) {
+				Log.e(TAG, e.toString());
+				e.printStackTrace();
+				return "Failed to post";
+			}
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			Toast.makeText(CloudActivity.this, result, Toast.LENGTH_LONG)
+					.show();
+		}
+
+		@Override
+		protected void onProgressUpdate(Integer... values) {
+			super.onProgressUpdate(values);
+		}
+
+	}
+
+}
---
+++ src/com/yamba/MainActivity.java
@@ -0,0 +1,103 @@
+package com.yamba;
+
+import winterwell.jtwitter.Twitter;
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity implements OnClickListener,
+		TextWatcher {
+
+	private static final String TAG = "StatusActivity";
+	private final int MAX_LENGTH = 140;
+	EditText editText;
+	Button button;
+	TextView textCount;
+	Twitter twitter;
+
+	@SuppressWarnings("deprecation")
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		editText = (EditText) findViewById(R.id.editText);
+		button = (Button) findViewById(R.id.buttonUpdate);
+		textCount = (TextView) findViewById(R.id.textCount);
+		button.setOnClickListener(this);
+
+		textCount.setText(Integer.toString(MAX_LENGTH));
+		textCount.setTextColor(Color.GREEN);
+		editText.addTextChangedListener(this);
+
+		twitter = new Twitter("student", "password"); //
+		twitter.setAPIRootUrl("http://yamba.marakana.com/api");
+
+	}
+
+	@Override
+	public void onClick(View v) {
+		twitter.setStatus(editText.getText().toString()); //
+		Log.d(TAG, "onClicked");
+	}
+
+	@Override
+	public void afterTextChanged(Editable editText) {
+		int count = MAX_LENGTH - editText.length();
+		textCount.setText(Integer.toString(count));
+		if (count >= 30) {
+			textCount.setTextColor(Color.GREEN);
+		} else if ((count < 30) && (count > 10)) {
+			textCount.setTextColor(Color.YELLOW);
+		} else {
+			textCount.setTextColor(Color.RED);
+		}
+
+	}
+
+	@Override
+	public void beforeTextChanged(CharSequence s, int start, int count,
+			int after) {
+	}
+
+	@Override
+	public void onTextChanged(CharSequence s, int start, int before, int count) {
+	}
+
+	class PostToTwitter extends AsyncTask<String, Integer, String> {
+
+		@Override
+		protected String doInBackground(String... params) {
+			try {
+				winterwell.jtwitter.Status status = twitter
+						.updateStatus(params[0]);
+				return status.text;
+			} catch (Exception e) {
+				Log.e(TAG, e.toString());
+				e.printStackTrace();
+				return "Failed to post";
+			}
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			Toast.makeText(MainActivity.this, result, Toast.LENGTH_LONG).show();
+		}
+
+		@Override
+		protected void onProgressUpdate(Integer... values) {
+			super.onProgressUpdate(values);
+		}
+
+	}
+}
